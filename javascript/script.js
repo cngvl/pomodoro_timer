883c5483a2e76f4a585d0d9bf5191772b44eb23e
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-04-24 16:19:49 +1000

    locked start button to prevent multiple timers being made at the same time

--- javascript/script.js
+++ javascript/script.js
@@ -15,20 +15,26 @@ startButton.addEventListener('click', () => {
   // alert("Start button clicked")
   // workTimer(currentTime)
   var workTimerDuration = parseInt(document.getElementById("timer-input-value").innerHTML, 10) * 60 // Currently in SECONDS
-  workTimerCountdown(workTimerDuration)
+  if (timerActive === false) {
+    workTimerCountdown(workTimerDuration)
+    timerActive = true
+    startButton.classList.toggle("active-timer")
+  } else {
+    console.log('There is already a timer active');
+  }
 })
 
-var timerOn = true;
+var timerActive = false
 
 function workTimerCountdown(workTimerDuration) {
   setInterval(() => {
     workTimerDuration -= 1
     // console.log(workTimerDuration);
-    if (workTimerDuration >= 0 && timerOn === true)  {
+    if (workTimerDuration >= 0) {
       var minutes = Math.floor(workTimerDuration / 60)
       var seconds = Math.floor(workTimerDuration % 60)
-      document.getElementById("timer-minutes").innerHTML = minutes;
-      document.getElementById("timer-seconds").innerHTML = seconds;
+      document.getElementById("timer-minutes").innerHTML = minutes + "mins";
+      document.getElementById("timer-seconds").innerHTML = seconds + "seconds";
     } else {
       document.getElementById("timer-minutes").innerHTML = "donezo";
       document.getElementById("timer-seconds").innerHTML = "beep beep ";
