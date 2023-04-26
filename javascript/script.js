94cd8b8de263ef1cf89831c63f3ddad0caaa9fb9
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-04-26 20:05:46 +1000

    core countdown timer functions working - need to add looping logic

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,17 @@
 const startButton = document.querySelector('[data-button-start]')
 const pauseButton = document.querySelector('[data-button-pause]')
 const resetButton = document.querySelector('[data-button-reset]')
+const continueButton = document.querySelector('[data-button-continue]')
+var workCycleText = document.getElementById("work-cycle-counter")
 
 // const integerRegex = /\d+/;
 // const match = string.match(regex);
 // const value = parseInt(match[0]);
 
 // console.log(value); // 25
+// TODO: Change input from static to user input
 
 // Can be used to track total number of loops
-// var timerCycles = 0
 
 var workTimerDuration = parseInt(document.getElementById("timer-input-value").innerHTML, 10) * 60 // Currently in SECONDS
 
@@ -25,34 +27,46 @@ startButton.addEventListener('click', () => {
 })
 
 var timerActive = false
+var timerPause = false
+// TODO: Can display # of workCycleCounter and cycle short with long break if % 4 === 0
 
 function workTimerCountdown(workTimerDuration) {
+  var workCycleCounter = 0
+  workCycleCounter += 1
   var timer = setInterval(() => {
+    workCycleText.innerHTML = workCycleCounter
     workTimerDuration -= 1
-    testTimeValue = workTimerDuration
+    pauseTimeValue = workTimerDuration
     // console.log(workTimerDuration);
     var minutes = Math.floor(workTimerDuration / 60)
     var seconds = Math.floor(workTimerDuration % 60)
-    if (workTimerDuration >= 0 && timerActive === true) {
+    if (workTimerDuration >= 0 && timerActive === true && timerPause === false) {
       document.getElementById("timer-minutes").innerHTML = minutes + "mins";
       document.getElementById("timer-seconds").innerHTML = seconds + "seconds";
-      console.log('timer tick - if');
+      // console.log('timer tick - if');
     }
-      else {
-    //   document.getElementById("timer-minutes").innerHTML = "donezo";
-    //   document.getElementById("timer-seconds").innerHTML = "beep beep ";
+    else {
+      workCycleText.innerHTML = workCycleCounter
+      //   document.getElementById("timer-minutes").innerHTML = "donezo";
+      //   document.getElementById("timer-seconds").innerHTML = "beep beep ";
       console.log('timer tick - else');
       clearInterval(timer)
-      console.log(workTimerDuration);
+      // console.log(pauseTimeValue + 1);
     };
-  // }, 1000);
+    // }, 1000); // TODO: Reset back to 1s timer
   }, 100);
 }
 
 pauseButton.addEventListener('click', () => {
-  // clearInterval(window.timer)
   // console.log('Pause clicked');
-  console.log(testTimeValue);
+  timerPause = true
+  console.log( `pauseTimeValue - ${pauseTimeValue}`);
+})
+
+continueButton.addEventListener('click', () => {
+  // console.log('Continue button pressed');
+  timerPause = false
+  workTimerCountdown(pauseTimeValue + 1)
 })
 
 resetButton.addEventListener('click', () => {
