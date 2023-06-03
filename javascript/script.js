a1086e2429c52ce46655ca78b8219e52400201d2
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-03 15:23:39 +1000

    javascript/script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,9 @@
-const startButton = document.querySelector('[data-button-start]')
-const pauseButton = document.querySelector('[data-button-pause]')
-const resetButton = document.querySelector('[data-button-reset]')
-const endButton = document.querySelector('[data-button-end]')
-const continueButton = document.querySelector('[data-button-continue]')
-var workCycleText = document.getElementById("work-cycle-counter")
+const startButton = document.querySelector("[data-button-start]");
+const pauseButton = document.querySelector("[data-button-pause]");
+const resetButton = document.querySelector("[data-button-reset]");
+const endButton = document.querySelector("[data-button-end]");
+const continueButton = document.querySelector("[data-button-continue]");
+var workCycleText = document.getElementById("work-cycle-counter");
 
 // const integerRegex = /\d+/;
 // const match = string.match(regex);
@@ -14,12 +14,16 @@ var workCycleText = document.getElementById("work-cycle-counter")
 
 // Can be used to track total number of loops
 
-var workTimerDuration = parseInt(document.getElementById("timer-input-work-value").innerHTML, 10) * 60 // Currently in SECONDS
-var breakTimerDuration = parseInt(document.getElementById("timer-input-break-value").innerHTML, 10) * 60 // Currently in SECONDS
+var workTimerDuration =
+  parseInt(document.getElementById("timer-input-work-value").innerHTML, 10) *
+  60; // Currently in SECONDS
+var breakTimerDuration =
+  parseInt(document.getElementById("timer-input-break-value").innerHTML, 10) *
+  60; // Currently in SECONDS
 
-var timerActive = false
-var timerPause = false
-var endSession = false
+var timerActive = false;
+var timerPause = false;
+var endSession = false;
 // TODO: Can display # of workCycleCounter and cycle short with long break if % 4 === 0
 
 // some aysnc? function await?
@@ -45,66 +49,70 @@ function resetState() {
 function timerCountdown(timerDuration) {
   // var workCycleCounter = 0
   // workCycleCounter += 1
-  clearInterval(timer)
+  clearInterval(timer);
   var timer = setInterval(() => {
     // workCycleText.innerHTML = workCycleCounter
-    timerDuration -= 1
-    pauseTimeValue = timerDuration
+    timerDuration -= 1;
+    pauseTimeValue = timerDuration;
     // console.log(timerDuration);
-    var minutes = Math.floor(timerDuration / 60)
-    var seconds = Math.floor(timerDuration % 60)
+    var minutes = Math.floor(timerDuration / 60);
+    var seconds = Math.floor(timerDuration % 60);
     if (timerDuration >= 0 && timerActive === true && timerPause === false) {
       // console.log('timer tick - if');
       document.getElementById("timer-minutes").innerHTML = minutes + "mins";
       document.getElementById("timer-seconds").innerHTML = seconds + "seconds";
-    }
-    else {
+    } else {
       // workCycleText.innerHTML = workCycleCounter
-      console.log('timer tick - else');
-      clearInterval(timer)
-    };
+      console.log("timer tick - else");
+      clearInterval(timer);
+    }
+    // console.log("Test String");
     // }, 1000); // TODO: Reset back to 1s timer
   }, 100);
 }
 
-startButton.addEventListener('click', () => {
+startButton.addEventListener("click", () => {
   // console.log('Start clicked');
   if (timerActive === false) {
-    timerActive = true
-    console.log('startButton timerCountdown starting');
-    timerCountdown(workTimerDuration)
+    timerActive = true;
+    console.log("startButton timerCountdown starting");
+    timerCountdown(workTimerDuration);
   } else {
-    console.log('There is already a timer active');
+    console.log("There is already a timer active");
   }
-})
+});
 
-pauseButton.addEventListener('click', () => {
+pauseButton.addEventListener("click", () => {
   // console.log('Pause clicked');
-  timerPause = true
-  console.log( `pauseTimeValue - ${pauseTimeValue}`);
-})
+  timerPause = true;
+  // console.log(`pauseTimeValue - ${pauseTimeValue}`);
+});
 
-continueButton.addEventListener('click', () => {
+continueButton.addEventListener("click", () => {
   // console.log('Continue button pressed');
-  timerPause = false
-  timerCountdown(pauseTimeValue + 1)
-})
+  timerPause = false;
+  timerCountdown(pauseTimeValue + 1);
+});
 
-resetButton.addEventListener('click', () => {
+resetButton.addEventListener("click", () => {
   // console.log('resetButton pressed');
   if (timerActive === true) {
-    resetState()
-    workTimerDuration = parseInt(document.getElementById("timer-input-work-value").innerHTML, 10) * 60
+    resetState();
+    workTimerDuration =
+      parseInt(
+        document.getElementById("timer-input-work-value").innerHTML,
+        10
+      ) * 60;
     // Can probably refactor this section
-    var minutes = Math.floor(workTimerDuration / 60)
-    var seconds = Math.floor(workTimerDuration % 60)
+    var minutes = Math.floor(workTimerDuration / 60);
+    var seconds = Math.floor(workTimerDuration % 60);
     document.getElementById("timer-minutes").innerHTML = minutes + "mins";
     document.getElementById("timer-seconds").innerHTML = seconds + "seconds";
     //
   } else {
-    console.log('No active timer to reset');
+    console.log("No active timer to reset");
   }
-})
+});
 
 // endButton.addEventListener('click', () => {
 //   console.log('endButton pressed');
